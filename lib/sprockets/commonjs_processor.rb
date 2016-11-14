c08c3df758659c10b55aea5425192af7689ab0fe
liceova22@gmail.com <liceova22@gmail.com>
2016-11-14 14:14:52 -0500

    Fixes

--- lib/sprockets/commonjs_processor.rb
+++ lib/sprockets/commonjs_processor.rb
@@ -28,10 +28,9 @@ module Sprockets
     end
 
     def call(input)
-      byebug
       if commonjs_module?(input)
         required  = Set.new(input[:metadata][:required])
-        required << input[:environment].resolve("commonjs-require.js")[0]
+        #required << input[:environment].resolve("commonjs-require.js")[0]
 
         nested_requires = resolve_requires(input)
         required = required + nested_requires
